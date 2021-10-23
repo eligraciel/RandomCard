4b1509d5d2c4831713a25740b0d7b6fa355040ff
eligraciel <eliana.bravo@gmail.com>
2021-10-23 18:15:45 +0000

    ajustes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let img = ["♦", "♥", "♠", "♣"];
+  let num = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  let pag = document.querySelector("body");
+  pag.style.background = "green";
+
+  let pinta;
+  let numCard;
+
+  pinta = Math.floor(Math.random() * img.length);
+  numCard = Math.floor(Math.random() * num.length);
+
+  if (img[pinta] == "♥" || img[pinta] == "♦") {
+    document.querySelector("#top").className = "cardRed";
+    document.querySelector("#bottom").className = "cardRed";
+  } else {
+    document.querySelector("#top").className = "cardBlack";
+    document.querySelector("#bottom").className = "cardBlack";
+  }
+
+  document.querySelector("#top").innerHTML = img[pinta];
+  document.querySelector("#center").innerHTML = num[numCard];
+  document.querySelector("#bottom").innerHTML = img[pinta];
+
+  console.log(pinta);
+  console.log(numCard);
 };
